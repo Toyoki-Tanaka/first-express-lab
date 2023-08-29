b94847b102a203dda65c64e7ca982dc5f4f76f22
SamteaSilver <tanakatoyoki19@gmail.com>
2023-08-29 10:34:09 -0500

    Magic 8 Ball

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ app.get("/tip/:total/:tipPercentage", (total, tipPercentage) => {
 })
 
 const quotes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-const randomElement = Math.floor(Math.random() * quotes.length)
+
+
 app.get("/magic/:quote", (request, response) => {
-    response.send(`<h1>${quotes[randomElement]}</h1>`)
+    const randomElement = Math.floor(Math.random() * quotes.length)
+    response.send(`<h1>${request.params.quote}</h1> <h1>${quotes[randomElement]}</h1>`)
 })
 
 
 app.listen(3000, () => {
-
 })
 
